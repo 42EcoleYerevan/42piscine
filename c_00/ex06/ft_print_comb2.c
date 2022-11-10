67aa9e17338ce57cfa84631c4a85717450fa12c8
Adamsonbor <adamsonbor@mail.ru>
2022-11-10 23:42:13 +0500

    c_00 fix ex06

--- c_00/ex06/ft_print_comb2.c
+++ c_00/ex06/ft_print_comb2.c
@@ -1,28 +1,53 @@
 #include <unistd.h>
 
 void ft_print_comb2(void);
+void combine(char f1, char f2, char s1, char s2);
+void print_numbers(char f1, char f2, char s1, char s2);
 
-int main() {
-    ft_print_comb2();
-    return 0;
+void ft_print_comb2(void)
+{
+	char f1;
+	char f2;
+	char s1;
+	char s2;
+
+	f1 = '0';
+	f2 = '0';
+	s1 = '0';
+	s2 = '0';
+	combine(f1, f2, s1, s2);
 }
 
-void ft_print_comb2(void) {
-    int space = 32;
-    int comma = 44;
-    for (int f1 = 48; f1 <= 57; f1++) {
-        for (int f2 = 48; f2 <= 56; f2++) {
-            for (int s1 = f1; s1 <= 57; s1++) {
-                for (int s2 = f2 + 1; s2 <= 57; s2++) {
-                    write(1, &f1, 1);
-                    write(1, &f2, 1);
-                    write(1, &space, 1);
-                    write(1, &s1, 1);
-                    write(1, &s2, 1);
-                    write(1, &comma, 1);
-                    write(1, &space, 1);
-                }
-            }
-        }
-    }
+void print_numbers(char f1, char f2, char s1, char s2) 
+{
+	write(1, &f1, 1);
+	write(1, &f2, 1);
+	write(1, " ", 1);
+	write(1, &s1, 1);
+	write(1, &s2, 1);
+	write(1, ", ", 2);
+}
+
+void combine(char f1, char f2, char s1, char s2)
+{
+	while (f1 <= '9')
+	{
+		f2 = '0';
+		while (f2 <= '8')
+		{
+			s1 = f1;
+			while (s1 <= '9')
+			{
+				s2 = f2 + 1;
+				while (s2 <= '9')
+				{
+					print_numbers(f1, f2, s1, s2);
+					s2++;
+				}
+				s1++;
+			}
+			f2++;
+		}
+		f1++;
+	}
 }
